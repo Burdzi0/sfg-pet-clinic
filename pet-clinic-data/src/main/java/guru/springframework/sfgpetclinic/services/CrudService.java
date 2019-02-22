c1944ace89752fc480595ded9f1730587330128e
Burdzi0 <burdzi0@programmer.net>
2019-02-22 16:46:34 +0100

    Add static resources, closes #17

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -4,9 +4,12 @@ import java.util.Set;
 
 public interface CrudService<T, R> {
 	Set<T> findAll();
+
 	T findById(R id);
+
 	T save(T object);
 
 	void delete(T object);
+
 	void deleteById(R id);
 }
